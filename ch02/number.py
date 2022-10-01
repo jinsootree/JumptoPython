bc2357030ecc4debb7b5db74a289119c81069a32
윤진수 <yjs104710@gmail.com>
2022-10-02 01:36:34 +0900

    commit

---
+++ ch02/number.py
@@ -0,0 +1,18 @@
+a = 123
+a = 0x8ff
+a = 4.24E10
+
+a = 3
+b = 4
+
+print(a + b)
+print(a * b)
+print(a / b)
+print(a ** b)
+print(7 % 3)
+print(7 / 3)
+print(7 // 3)
+print(14 / 3) # 몫 반환
+print(14 // 3) # 몫 반환 (정수)
+print(14 % 3) # 나머지 반환
+print(a)
