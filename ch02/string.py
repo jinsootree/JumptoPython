aba20fd964fd9c4142a7b9a0b6328e797fa3f78d
윤진수 <yjs104710@gmail.com>
2022-10-03 00:10:14 +0900

    commit

---
+++ ch02/string.py
@@ -0,0 +1,71 @@
+food = "python's favorite food is perl"
+print(food)
+
+say ='"python is very easy." he says'
+print(say)
+
+food = 'python\'s favorite food is perl'
+print(food)
+
+say = "\"python is very easy.\" he says"
+print(say)
+
+multiline = "Life is too short\nyou need python"
+print(multiline)
+
+multiline = """
+Life is too short
+tyou need python\
+"""
+print(multiline)
+
+head = "python"
+tail = " is fun!"
+
+print(head + tail)
+
+a = "python "
+a = a * 2
+print(a)
+
+print("=" * 50)
+print("my program")
+print("=" * 50)
+
+a = "life is too short, you need python"
+print(a[3])
+
+print(a[0])
+print(a[12])
+print(a[-1])
+
+print(a[0:4])
+print(a[0:3])
+
+print(a[0:5])
+
+print(a[0:2])
+print(a[5:7])
+print(a[12:17])
+
+print(a[15:])
+print(a[:13])
+
+print(a[:])
+print(a[19:-7])
+
+a = "20010331Rainy"
+date = a[:8]
+weather = a[8:]
+print(date)
+print(weather)
+
+a = "20010331Rainy"
+year = a[:4]
+date = a[4:8]
+weather = a[8:]
+
+print(year)
+print(date)
+print(weather)
+
